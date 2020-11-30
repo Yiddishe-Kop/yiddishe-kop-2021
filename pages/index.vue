c0e864413a5cf80deafa7baac333101a241d7dae
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-11-30 19:14:24 +0200

    fix: randomImage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <div class="absolute top-0 left-0 right-0 z-0 w-full">
-      <img :src="`https://source.unsplash.com/${unsplashImages[Math.randomBetween(0, unsplashImages.length - 1)]}`" class="object-cover w-full">
-      <div class="absolute inset-0 bg-gray-200 bg-opacity-70 bg-gradient-to-t from-gray-200"></div>
+      <img
+        v-if="coverImage"
+        :src="`https://source.unsplash.com/${coverImage}`"
+        class="object-cover w-full"
+      />
+      <div
+        class="absolute inset-0 bg-gray-200 bg-opacity-70 bg-gradient-to-t from-gray-200"
+      ></div>
     </div>
 
     <div class="relative my-16 text-center">
@@ -12,27 +18,33 @@
       </h4>
       <div class="flex justify-center mt-6 space-x-4">
         <nuxt-link to="/about" class="btn btn-lg">More about me</nuxt-link>
-        <nuxt-link to="/writing" class="btn btn-lg btn-brand">Writing</nuxt-link>
+        <nuxt-link to="/writing" class="btn btn-lg btn-brand"
+          >Writing</nuxt-link
+        >
       </div>
     </div>
 
-    <section class="relative my-12">
-    </section>
+    <section class="relative my-12"></section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      unsplashImages: [
-        '4rx3NLmWaZk',
-        'sdqIx9JQiTw',
-        'AeqGXkYWIHM',
-        'kbYD_aSAeg8'
-      ]
-    }
-  }
+      coverImage: null,
+    };
+  },
+  created() {
+    const unsplashImages = [
+      "4rx3NLmWaZk",
+      "sdqIx9JQiTw",
+      "AeqGXkYWIHM",
+      "kbYD_aSAeg8",
+    ];
+    this.coverImage =
+      unsplashImages[Math.randomBetween(0, unsplashImages.length - 1)];
+  },
 };
 </script>
