45a37d7e4eabfad667b6874f7cae0fcc947ca393
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-03-13 23:39:38 +0200

    feat: articleImage component

---
+++ components/global/ArticleImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <figure>
+    <nuxt-picture placeholder :src="src" class="article-img" />
+    <figcaption v-if="caption" class="text-center">{{ caption }}</figcaption>
+  </figure>
+</template>
+
+<script>
+export default {
+  name: 'ArticleImage',
+  props: {
+    src: String,
+    caption: String,
+  },
+}
+</script>
+
+<style lang="scss">
+.article-img {
+  img {
+    @apply rounded-md mt-2 mb-0;
+  }
+}
+</style>
