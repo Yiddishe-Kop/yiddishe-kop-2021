01d71cee55ba2cb2ce93d99d17d28333ae133b8a
Yehuda Neufeld <yehuda@hybiz.co.il>
2022-10-31 11:51:08 +0200

    chore: update deps

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,13 @@
 import colors from 'tailwindcss/colors'
-import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+  target: 'static',
+
+  /*
+   ** Global CSS
+   */
+  css: ['@/assets/css/fonts.scss', '@/assets/css/global.scss', '@/assets/css/transitions.scss'],
+
   /*
    ** Headers of the page
    */
@@ -16,32 +22,11 @@ export default defineNuxtConfig({
    */
   loading: { color: colors.amber[400] },
 
-  /**
-   * ENV vars
-   */
-  publicRuntimeConfig: {
-    contactFormSubmissionUrl:
-      process.env.NODE_ENV == 'production'
-        ? 'https://portal.yiddishe-kop.com/api/contact'
-        : 'http://yk-portal.test/api/contact',
-  },
-
-  /*
-   ** Global CSS
-   */
-  css: ['~/assets/css/fonts.scss', '~/assets/css/global.scss', '~/assets/css/transitions.scss'],
-
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    '@nuxtjs/pwa',
-    '@nuxt/image-edge',
-    '@nuxt/content',
-    '@nuxtjs/feed',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
+  modules: ['@nuxt/image-edge', '@nuxt/content'],
+  buildModules: ['@nuxtjs/tailwindcss'],
 
   image: {
     provider: 'auto',
@@ -57,72 +42,28 @@ export default defineNuxtConfig({
     },
   },
 
-  feed: [
-    {
-      path: '/feed.xml',
-      async create(feed) {
-        const hostname = process.NODE_ENV === 'production' ? 'https://yiddishe-kop.com' : 'http://localhost:3000'
-
-        feed.options = {
-          title: 'Yiddishe-Kop tech blog',
-          description: 'Deep dives into programming topics',
-          link: `${hostname}/feed.xml`,
-        }
-
-        const { $content } = require('@nuxt/content')
-
-        const posts = await $content('articles').sort({ createdAt: -1 }).sort({ createdAt: -1 }).fetch()
-
-        posts.forEach(post => {
-          const url = `${hostname}/articles/${post.slug}`
-          feed.addItem({
-            title: post.title,
-            id: url,
-            link: url,
-            date: new Date(post.createdAt),
-            description: post.description,
-            content: post.bodyPlainText.slice(0, 50),
-          })
-        })
-
-        feed.addContributor({
-          name: 'Yehuda Neufeld',
-          email: 'dont@email.me',
-          link: 'https://yiddishe-kop.com/',
-        })
-      },
-      cacheTime: 1000 * 60 * 15,
-      type: 'rss2',
+  /**
+   * ENV vars
+   */
+  runtimeConfig: {
+    public: {
+      contactFormSubmissionUrl:
+        process.env.NODE_ENV == 'production'
+          ? 'https://portal.yiddishe-kop.com/api/contact'
+          : 'http://yk-portal.test/api/contact',
     },
-  ],
+  },
 
   googleAnalytics: {
     id: 'UA-143315552-2',
   },
 
-  /**
-   * PWA config
-   */
-  pwa: {
-    meta: {
-      name: 'Yiddishe Kop',
-      description: 'Cutting edge software development',
-      author: 'Yehuda Neufeld',
-    },
-    manifest: {
-      name: 'Yiddishe Kop',
-      short_name: 'Yiddishe Kop',
-      description: 'Cutting edge software development',
-      background_color: colors.amber[400],
-    },
-  },
-
-  nitro: {
-    plugins: ['~/server/plugins/content.ts'],
-  },
-
   /*
    ** Build configuration
    */
-  build: {},
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    },
+  },
 })
