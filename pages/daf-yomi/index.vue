3e376121d8e5e08c7779da19b0a5cbe2b1eeddc6
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2024-03-01 14:03:03 +0200

    wip: privacy policy

--- pages/daf-yomi/index.vue
+++ pages/daf-yomi/index.vue
@@ -3,9 +3,10 @@
     <Head>
       <Title>Daf Yomi App | Yiddishe Kop</Title>
     </Head>
-    <!-- Hero section -->
-    <h1>Daf Yomi iOS app</h1>
-    <nuxt-link to="/daf-yomi/privacy">Privacy Policy</nuxt-link>
+    <article class="prose mx-auto px-2 py-6">
+      <h1>Daf Yomi iOS app</h1>
+      <nuxt-link to="/daf-yomi/privacy">Privacy Policy</nuxt-link>
+    </article>
   </div>
 </template>
 
--- pages/daf-yomi/privacy.vue
+++ pages/daf-yomi/privacy.vue
@@ -3,9 +3,51 @@
     <Head>
       <Title>Daf Yomi App | Yiddishe Kop</Title>
     </Head>
-    <!-- Hero section -->
-    <h1>Daf Yomi iOS app Privacy Policy</h1>
-    <nuxt-link to="/daf-yomi">App info</nuxt-link>
+
+    <article class="prose mx-auto px-2 py-6">
+      <h2>Daf Yomi iOS app</h2>
+      <h1>Privacy Policy</h1>
+      <p>
+        Daf Yomi respects your privacy and is committed to protecting any personal information that you may provide
+        while using our app. This Privacy Policy outlines how we collect, use, and safeguard your information.
+      </p>
+      <h3>Information Collection and Use</h3>
+      <p>
+        Daf Yomi does not collect any personal information or data from users. We do not require you to provide any
+        personal information to use our app.
+      </p>
+      <h3>Non-Personal Information</h3>
+      <p>
+        We may collect non-personal information automatically as you use our app. This may include technical information
+        such as device information, usage statistics, and diagnostic data. However, this information is collected in a
+        way that does not personally identify you and is used solely for the purpose of improving our app's performance
+        and user experience.
+      </p>
+      <h3>Third-Party Services</h3>
+      <p>
+        Our app may integrate with third-party services, such as analytics providers or advertising networks. However,
+        since we do not collect any personal information, these third-party services do not have access to any user data
+        through our app.
+      </p>
+      <h3>Children's Privacy</h3>
+      <p>
+        Daf Yomi is not intended for use by children under the age of 13. We do not knowingly collect personal
+        information from children. If you are a parent or guardian and believe that your child has provided us with
+        personal information, please contact us so that we can delete such information from our systems.
+      </p>
+      <h3>Changes to This Privacy Policy</h3>
+      <p>
+        We reserve the right to update or change our Privacy Policy at any time. Any changes will be reflected in this
+        document, and we encourage you to review this Privacy Policy periodically for any updates.
+      </p>
+      <h3>Contact Us</h3>
+      <p>
+        If you have any questions or concerns about our Privacy Policy, please contact us at
+        <a href="mailto:daf-yomi-app@yiddishe-kop.com">daf-yomi-app@yiddishe-kop.com</a>.
+      </p>
+      <blockquote>This Privacy Policy was last updated on 01/03/2024.</blockquote>
+      <nuxt-link to="/daf-yomi">App info</nuxt-link>
+    </article>
   </div>
 </template>
 
