02e244cd962028d5221633205c745a4caf755157
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2025-07-21 23:26:44 +0300

    ui

--- pages/apps/daf-yomi/index.vue
+++ pages/apps/daf-yomi/index.vue
@@ -3,10 +3,10 @@
     <Title>Apps | Daf Yomi Tracker</Title>
   </Head>
   <div dir="rtl">
-    <div class="relative !my-16 text-center !block px-1">
+    <div class="relative my-16 text-center !block px-1">
       <p class="text-gray-500 font-siddur text-2xl leading-none">בעזהשי״ת גם אני עושה</p>
       <h1
-        class="font-siddur text-9xl -mt-10"
+        class="font-siddur text-8xl sm:text-9xl leading-[0.65em]"
         :style="{
           '--siddur-weight': 900,
           '--siddur-width': 400,
@@ -15,7 +15,7 @@
         סיום הש״ס
       </h1>
     </div>
-    <ul class="columns-3 my-8">
+    <ul class="columns-2 md:columns-3 my-24">
       <li v-for="(dafim, masechet) in Shas.MASECHTOS" :key="masechet" class="break-inside-avoid-column">
         <MasechetGrid :masechet="masechet" :pages="dafim" />
       </li>
